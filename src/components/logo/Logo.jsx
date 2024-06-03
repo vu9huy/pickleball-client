ff89cbc9d7b2e0e1a46ebedd3ea6ff4ad6b697de
vu9huy <quan2704vu@gmail.com>
2024-06-03 21:31:15 +0700

    1

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Logo.module.css";
+import Image from "next/image";
 
 const Logo = () => {
 
     return (
         <div className={styles["logo-container"]}>
-            Logo
+            <Image src={"/logo-fit-96x96.webp"} alt="logo" width={50} height={50} />
         </div>
     );
 };
